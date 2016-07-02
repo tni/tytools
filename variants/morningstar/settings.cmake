53df3ae104d8e36b303ee56761c52454d367cf57
Niels Martignène <niels.martignene@gmail.com>
2016-07-02 20:44:02 +0200

    Add Morningstar Engineering Updater build variant

---
+++ variants/morningstar/settings.cmake
@@ -0,0 +1,8 @@
+set(TY_CONFIG_TYC_BUILD OFF CACHE BOOL "")
+set(TY_CONFIG_TYQT_BUILD OFF CACHE BOOL "")
+
+set(TY_CONFIG_UPTY_EXECUTABLE "Morningstar Engineering Updater" CACHE STRING "")
+set(TY_CONFIG_UPTY_NAME "Morningstar Engineering Updater" CACHE STRING "")
+
+set(TY_CONFIG_URL_WEBSITE "http://www.morningstarfx.com/" CACHE STRING "")
+set(TY_CONFIG_URL_BUGS "" CACHE STRING "")
